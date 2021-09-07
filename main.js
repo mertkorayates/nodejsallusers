f724758ebaf2eec0c19fc5c508bcbb513684d6f2
mertkorayates <mertkorayates200@gmail.com>
2021-09-07 17:20:03 +0300

    aa
    bb

---
+++ main.js
@@ -0,0 +1,41 @@
+express = require("express")
+path = require("path")
+bodyParser = require("body-parser")
+app = express();
+var kullanici = [];
+var views = path.join(__dirname, 'views');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'pug');
+ 
+
+
+app.post("/kayit/:id/:isim/:soyisim",(req,res)=>{
+    console.log("kullanici kayit edildi")
+    user = {
+        id : req.params.id,
+        isim : req.params.isim,
+        soyisim : req.params.soyisim,
+    }
+
+
+
+    kullanici.push(user);
+
+
+    res.send({
+        id : req.params.id,
+        isim : req.params.isim,
+        soyisim : req.params.soyisim,
+    })
+})
+
+
+app.get("/ekle",(req,res)=>{
+  
+res.send({kullanici})
+
+})
+
+app.listen(3000,()=>{
+console.log("Port Open")
+})
